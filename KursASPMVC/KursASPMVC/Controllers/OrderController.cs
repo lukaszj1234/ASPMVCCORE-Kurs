4166b1bed93238a78a39119833a113c8ff43f458
lukaszj1234 <51384056+lukaszj1234@users.noreply.github.com>
2020-08-31 16:59:34 +0200

    Revert "+ Orders"
    This reverts commit a34ab462005add4cd74d9f12067e08e79851124e.

--- KursASPMVC/KursASPMVC/Controllers/OrderController.cs
+++ KursASPMVC/KursASPMVC/Controllers/OrderController.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using KursASPMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,35 +10,9 @@ namespace KursASPMVC.Controllers
     [Route("[controller]/[action]")]
     public class OrderController : Controller
     {
-        private IOrderRepository _repository;
-        private Cart _cart;
-
-        public OrderController(IOrderRepository repo, Cart cartService)
+        public ViewResult Checkout()
         {
-            _repository = repo;
-            _cart = cartService;
-        }
-        public IActionResult Checkout(Order order)
-        {
-            if (_cart.Lines.Count() == 0)
-            {
-                ModelState.AddModelError("", "Koszyk jest pusty");
-            }
-            if (ModelState.IsValid)
-            {
-                order.Lines = _cart.Lines.ToArray();
-                _repository.SaveOrder(order);
-                return RedirectToAction(nameof(Completed));
-            }
-            else
-            {
-                return View(order);
-            }
-        }
-        public ViewResult Completed()
-        {
-            _cart.Clear();
-            return View();
+            return View(new Order());
         }
     }
 }
